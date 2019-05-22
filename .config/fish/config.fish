08e8b38acbd7089c1d429d8deae04644ce6671c6
Khimtsov Anton Igorevich <projects.tlt@ya.ru>
2019-05-22 11:54:46 +0300

    fish: fix global working path; git: use nevim as editor and add two alias for log command

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,6 @@
 nvm use default --silent #fix for using last node version by default in nvm
 set -gx PATH $PATH ./node_modules/.bin
-set -gx GOPATH $HOME/Projects/Lamoda/go-projects
+set -gx GOPATH $HOME/Projects/Lamoda/go-site
 source $GOPATH/src/site/dev/go.env
 
 set fish_greeting ""
