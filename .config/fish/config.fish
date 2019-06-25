0f7dbac5b9327fb1c5c0aa41cf86d722715e0671
Khimtsov Anton Igorevich <projects.tlt@ya.ru>
2019-06-25 10:55:06 +0300

    fix problem with ag search

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,9 @@
 nvm use default --silent #fix for using last node version by default in nvm
 set -gx PATH $PATH ./node_modules/.bin
-set -gx GOPATH $HOME/projects
-set -gx FZF_DEFAULT_COMMAND $FZF_DEFAULT_COMMAND 'ag -l --nogroup  --nocolor --hidden -g ""'
-set -gx LANG $LANG en_US.UTF-8
+set -gx GOPATH $HOME/Projects/Lamoda/go-site
+set -gx FZF_DEFAULT_COMMAND 'Ag -l --nogroup  --nocolor --hidden -g ""'
+set -gx LANG en_US.UTF-8
+set -gx TMUX_GITBAR_DIR $HOME/.tmux/gitbar
 
 #set -gx NVM_DIR "$HOME/.nvm $NVM_DIR/nvm.sh"
 
