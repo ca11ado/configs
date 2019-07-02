8b80b97916492adf3e502147dad42c04ad723602
Khimtsov Anton Igorevich <projects.tlt@ya.ru>
2019-07-02 10:26:54 +0300

    fish: fix gitbar function

--- .config/fish/functions/gitbar_update.fish
+++ .config/fish/functions/gitbar_update.fish
@@ -1,5 +1,6 @@
 # In case of errors -- check variable. It can contains some extra stuff. 
 # It should contain path to the gitbar update file
 function gitbar_update --on-event fish_postexec --description 'Update tmux gitbar'
-  eval "$PROMPT_COMMAND"
+  #eval "$PROMPT_COMMAND"
+  $HOME/.tmux/gitbar/update-gitbar
 end
